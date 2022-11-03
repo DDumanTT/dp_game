aca818d7da6403711af3a3dcf48aea19737da297
DDumanTT <domantas.stakionis@ktu.edu>
2022-11-04 00:49:57 +0200

    Add server index.html

--- server/src/index.ts
+++ server/src/index.ts
@@ -23,7 +23,7 @@ import PlayerService from "./services/PlayerService";
 
 const io = new sockets.Server(server, { cors: { origin: "*" } });
 
-if (NODE_ENV == "production") {
+if (process.env.NODE_ENV == "production") {
   app.use(express.static("public"));
   app.get("/", (req, res) => res.sendFile("index.html"));
 }
