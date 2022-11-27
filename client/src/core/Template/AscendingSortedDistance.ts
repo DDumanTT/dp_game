b6c8790e54606478c97d42591f1325e5829c1f9a
modestaskris <modestaskris@gmail.com>
2022-11-27 22:11:10 +0200

    template fixed console.logs

--- client/src/core/Template/AscendingSortedDistance.ts
+++ client/src/core/Template/AscendingSortedDistance.ts
@@ -11,7 +11,6 @@ export class AscendingSortedDistances extends BaseDistance{
 
     protected Sort(): void {
         if(this._convertedPlayers !== undefined && this._convertedPlayers.length > 1){
-            console.log("Sorting..")
             this._convertedPlayers.sort((a: IDistanceUser, b: IDistanceUser) => b.distance - a.distance);
         }
     }
